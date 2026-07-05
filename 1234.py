1612d0c95168a1b845d686aabe7be0feaeba570e
Visvajit kasotiya - Vishwajit <139753306+kvisvajit1@users.noreply.github.com>
2026-07-06 00:59:28 +0530

    Create 1234.py

---
+++ 1234.py
@@ -0,0 +1,17 @@
+user_input = input("enter number separated by spaces:")
+
+Number = [Float(x) for x in used_input.split()]
+
+Total = 0
+count = 0
+
+for num in numbers
+total += num
+count += 1
+
+if count > 0
+average = total / count
+else : average = 0
+
+print("sum of the list=",total)
+print("average of the list=",avarage)
